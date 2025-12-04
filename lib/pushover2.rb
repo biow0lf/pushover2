fac0a6399acd4c0b9da3c20ff746dc60ee41be52
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-04 22:33:25 +0100

    Init with: bundle gem pushover2 --no-coc --no-ext --git --mit --test=rspec --ci=github --linter=standard

---
+++ lib/pushover2.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pushover2/version"
+
+module Pushover2
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/pushover2_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pushover2 do
+  it "has a version number" do
+    expect(Pushover2::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
