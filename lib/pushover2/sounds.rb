2303de81fb4d2befbb91c612a448d8bec4eccea5
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-06 21:13:45 +0100

    Update

--- lib/pushover2/sounds.rb
+++ lib/pushover2/sounds.rb
@@ -1,29 +1,29 @@
+# frozen_string_literal: true
+
 module Pushover2
-  module Sounds
-    SOUNDS = [
-      "pushover",
-      "bike",
-      "bugle",
-      "cashregister",
-      "classical",
-      "cosmic",
-      "falling",
-      "gamelan",
-      "incoming",
-      "intermission",
-      "magic",
-      "mechanical",
-      "pianobar",
-      "siren",
-      "spacealarm",
-      "tugboat",
-      "alien",
-      "climb",
-      "persistent",
-      "echo",
-      "updown",
-      "vibrate",
-      "none"
-    ].freeze
-  end
+  SOUNDS = [
+    "pushover",
+    "bike",
+    "bugle",
+    "cashregister",
+    "classical",
+    "cosmic",
+    "falling",
+    "gamelan",
+    "incoming",
+    "intermission",
+    "magic",
+    "mechanical",
+    "pianobar",
+    "siren",
+    "spacealarm",
+    "tugboat",
+    "alien",
+    "climb",
+    "persistent",
+    "echo",
+    "updown",
+    "vibrate",
+    "none"
+  ].freeze
 end
